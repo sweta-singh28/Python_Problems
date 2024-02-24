1057eace0ffb0143df617c2eb5fd606490c07dcd
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-24 22:03:52 +0530

    sweta

---
+++ Print_even_numbers_from_1_to_100.py
@@ -0,0 +1,5 @@
+#print all the even numbers from 1 to 100
+for i in range (100):
+    if (i % 2 == 0):
+        print(i)
+    
---
+++ Sum_of_all_even_numbers_from_1_to_100.py
@@ -0,0 +1,6 @@
+#print the sum of all even numbers from 1 to 100
+sum = 0
+for i in range (1, 100):
+    if (i % 2 == 0):
+        sum += i
+        print("Sum of all even number: ", sum)
