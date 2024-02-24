da1ff9739de8bf052e2cdbca7ac45d21ebd82c7d
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-25 00:37:39 +0530

    sweta

---
+++ Factorial_of_a_number_using_while_loop.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial is not defined for negative number.")
+elif num == 0:
+    print("The factorial of 0 is 1.")
+else:
+    factorial = 1
+    i = 1
+    while(i <= num):
+        factorial *= i
+        i += 1
+        print("The factorial of the given number is: ", factorial)
