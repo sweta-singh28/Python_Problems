d1716b46f44ed70d2059be56a31117220be72e4b
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-02 18:54:45 +0530

    sweta

---
+++ Trafic_light.py
@@ -0,0 +1,10 @@
+light = input("Enter the color of the light: ")
+if(light =="red"):
+    print("STOP")
+elif(light == "yellow"):
+    print("LOOK") 
+elif(light == "green"):
+    print("GO") 
+else:
+    print(("The light is broken"))      
+       
