c7011ee006dc41089e2893607b101abe0ce1ce01
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-05 20:19:11 +0530

    sweta

--- continue.py
+++ continue.py
@@ -4,5 +4,5 @@ while i < len(str):
     if str[i] == 'v' or str[i] == 'i':
         i+=1
         continue
-    print("The curreant letter is: ", str[i])
+    print("The curreant letter is:  ", str[i])
     i+=1
