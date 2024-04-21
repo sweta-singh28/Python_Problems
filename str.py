15c429e9f981541745b6f470f0df76539d7640df
sweta-singh28 <swetasinghgs28@gmail.com>
2024-04-21 23:12:06 +0530

    sweta

---
+++ str.py
@@ -0,0 +1,40 @@
+#len()
+name = "Sweta"
+print(len(name))
+
+#concatination
+namee = "Gungun"
+surname =  "Singh"
+concate = namee + " " + surname
+print(concate)
+
+#repeat (*)
+str1 = "joy"
+repeat = str1 * 2
+print(repeat)
+
+#lower() and upper()
+str = "Hello"
+print(str.lower())
+print(str.upper())
+
+#capitalize() and title()
+str2 = "sweta singh"
+print(str2.capitalize())
+print(str2.title())
+
+#count()
+str3 = "Swetaaa"
+print(str3.count('a'))
+
+str4 = "Hello guys"
+print(str4.replace('Hello', 'Hey'))
+
+#find()
+str5 = "Hello world"
+print(str5.find('world'))
+
+#split
+str6 = "Hello World"
+print(str6.split(","))
+
