12fe047e6d8ea5b7739c53e2a28d6e1f1f3aca71
sweta-singh28 <swetasinghgs28@gmail.com>
2024-04-29 20:23:24 +0530

    sweta

--- Smallest_of_two_numbers.py
+++ Smallest_of_two_numbers.py
@@ -1,6 +1,6 @@
 a = input("Enter the first number: ")
 b = input("Enter the second number: ")
 if(a<b):
-    print("The smallest number is ", a)
+    print("The smallest number is: ", a)
 else:
-    print("The smallest number is ", b)
+    print("The smallest number is: ", b)
