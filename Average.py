25f6d2718dbdee62eb55cc14efc0c19bf6280316
sweta-singh28 <swetasinghgs28@gmail.com>
2024-05-07 21:21:07 +0530

    sweta

--- Average.py
+++ Average.py
@@ -1,5 +1,5 @@
 #Average of two numbers
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
-print("The average of two number is: ", (a+b)/2)
+print("The average of two numbers is: ", (a+b)/2)
 print(type(a))
