b647f27b560bebfe0f7141e00d4620863eb709fa
sweta-singh28 <swetasinghgs28@gmail.com>
2024-05-23 23:17:32 +0530

    hm

--- MinNumberfromaList.py
+++ MinNumberfromaList.py
@@ -1,4 +1,4 @@
 #WAP to find ouit the maximum number from a list
 numbers = [1,5,45,67,2,0,8,1,34]
 min_number = min(numbers)
-print("The maximum number is: ", min_number)
+print("The maximum number is ", min_number)
